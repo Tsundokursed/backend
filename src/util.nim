4cdac3fd37a26666790706801380f74792faafe0
leventea <leventea@tuta.io>
2020-04-23 21:01:05 +0200

    Add get() and toSeq() procs

--- src/util.nim
+++ src/util.nim
@@ -3,8 +3,16 @@ import elvis
 template has*(key: string): bool =
   ## Returns wheter request.formData has the `key` key
   ?request.formData[key].body
+
+template get*(key: string): string =
+  request.formData[key].body
   
 template need*(key: string) =
   ## Returns Http400 if the specified key not set in formData
   if not has key:
     resp Http400, key & " field not set\n"
+
+proc toSeq*(str: string): seq[char] =
+  for c in str:
+    result.add(c)
+  
